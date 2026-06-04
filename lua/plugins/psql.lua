bd6bd74602d38c7e2bc9462c3670ba49ceedf98c
axonde <alex.chpak@gmail.com>
2026-06-04 14:06:51 +0300

    fix: rewrite the write new file function to take the case when parent directories aren't created yet

--- lua/plugins/psql.lua
+++ lua/plugins/psql.lua
@@ -18,35 +18,30 @@ local function loadJsonFromFile(filepath)
     return decoded_json
 end
 
-local function safeWrite(filename, content)
-    local file, err = io.open(filename, "w")
-    if not file then
-        print("Error opening file:", err)
-        return false
+local function createFileWithParentDirectories(path, content)
+    local folderPath = path:match("(.*)/")
+
+    if folderPath then
+        os.execute("mkdir -p " .. folderPath)
     end
 
-    local success, err = file:write(content)
-    if not success then
-        print("Error writing to file:", err)
-        file:close()
-        return false
-    end
-
+    local file = assert(io.open(path, "w"))
+    file:write(content)
     file:close()
-    return true
 end
 
 local connections_path = vim.fn.stdpath("config") .. "/keys/psql.json"
 
 if vim.fn.filereadable(connections_path) == 0 then
     vim.notify("PSQL config file do not exist, create a new one", vim.log.levels.WARN)
-    safeWrite(connections_path, "{}")
+    createFileWithParentDirectories(connections_path, "{}")
 end
 
 local connections = loadJsonFromFile(vim.fn.stdpath("config") .. "/keys/psql.json")
 
 return {
     "axonde/psql.nvim",
+    enabled = false, -- disabled for YANDEX
     cmd = { "Psql", "PsqlExec", "PsqlListDBs" },
     config = function()
         require("psql").setup({

4563dee261f5e85a367dfe40c88e72a1c496171a
axonde <alex.chpak@gmail.com>
2026-06-12 13:22:00 +0300

    fix: remove treesitter's explicit version set

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,14 +1,11 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    branch = 'master',
     lazy = false,
     build = ":TSUpdate",
-    config = function()
-        require'nvim-treesitter.configs'.setup {
-            auto_install = true,
-            highlight = {
-                enable = true
-            }
+    opts = {
+        auto_install = true,
+        highlight = {
+            enable = true
         }
-    end
+    }
 }

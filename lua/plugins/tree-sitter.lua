80c9120c5f31a65c970d6c08fe24c3112c1acd34
axonde <alex.chpak@gmail.com>
2026-06-13 12:58:23 +0300

    fix: remove image.nvim cause it ruines and throw everywhere

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,11 +1,9 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     lazy = false,
     build = ":TSUpdate",
     opts = {
         auto_install = true,
-        highlight = {
-            enable = true
-        }
     }
 }
